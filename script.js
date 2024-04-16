15beac3750638e3f3885ac1479d358ca14fe798d
Meredith Thompson <7479754-merethom@users.noreply.replit.com>
2024-04-16 19:39:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const d4 = Math.floor(Math.random() * (5 - 1) + 1)
+const d6 = Math.floor(Math.random() * (7 - 1) + 1)
+const d8 = Math.floor(Math.random() * (9 - 1) + 1)
+const d10 = Math.floor(Math.random() * (9 - 1) + 1)
+const d12 = Math.floor(Math.random() * (13 - 1) + 1)
+const d20 = Math.floor(Math.random() * (21 - 1) + 1)
+const d100Values = [00, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+const d100 = d100Values[Math.floor(Math.random() * (d100Values.length - 1) + 1)]
+
+const d1002 = Math.floor(Math.random() * (9 - 1) + 1) * 10
+
+console.log(d100); console.log(d1002);
+
+
+
+
+
